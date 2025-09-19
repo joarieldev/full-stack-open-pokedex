82e7d14adbfe19c51327c7d1e09b7b7ea24040c4
joariel <joariel17@gmail.com>
2025-09-19 18:02:39 -0300

    add part11 - 11.6

--- .eslintrc.js
+++ .eslintrc.js
@@ -43,7 +43,8 @@ module.exports = {
     "arrow-spacing": [
       "error", { "before": true, "after": true }
     ],
-    "no-console": "error",
-    "react/prop-types": 0
+    "no-console": "off",
+    "react/prop-types": 0,
+    "no-undef": "off"
   }
 }
